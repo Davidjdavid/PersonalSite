79c1ada87a19f7cf80107304858919318f94838e
David <dave.j.hazelwood@gmail.com>
2021-02-05 14:51:15 -0600

    .

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title', 'About Me')
+
+@section('content')
+    <body>
+        <p>This is the about me section</p>
+    </body>
+@endsection
---
+++ resources/views/projects.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title', 'Projects')
+
+@section('content')
+    <body>
+        <p>This is the projects section</p>
+    </body>
+@endsection
